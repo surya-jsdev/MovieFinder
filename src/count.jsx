33e55d5e19c0ba6887e7b50f125d67c60e80e887
SURYA-200346 <surya.jsdev@gmail.com>
2025-12-13 21:30:43 +0530

    fetch data pratices

--- src/count.jsx
+++ src/count.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-function count() {
-   
+import React, { useState } from 'react'
+import './index.css'
+
+function GetData() {
+    const [Result, setResults] = useState([]);
+    const [Error, setError] = useState(null);
+
+    async function fetchData() {
+        try {
+            const res = await fetch(
+                "https://68a582352a3deed2960dbd2c.mockapi.io/form/name"
+            );
+            const data = await res.json();
+            setResults(data);
+        } catch (err) {
+            setError("Something went wrong");
+        }
+    }
 
     return (
         <>
-           
+            <button onClick={fetchData} style={{ background: "black", padding: "10px", color: "white" }}>Click</button>
+            {Error && <p style={{ color: "red" }}>{Error}</p>}
+            <div className='container'>
+                {Result.map((item) => (
+                    <div key={item.id}>
+                        <img src={item.image} alt="Product-img" style={{width:"200px",height:"200px"}}/>
+                        <h3>{item.name}</h3>
+                        <p>{item.email}</p>
+                    </div>
+                ))}
+            </div>
+
+
         </>
+
     )
 }
 
-export default count
+export default GetData
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Count from './count'
+import GetData from './count'
 import './index.css'
 import App from './App.jsx'
 import ColorSchemesExample from './Navbar.jsx'
@@ -8,9 +8,9 @@ import ColorSchemesExample from './Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
 
     {/* <ColorSchemesExample /> */}
-    {/* <Count /> */}
+    <GetData />
   </StrictMode>,
 )
